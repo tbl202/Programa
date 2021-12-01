67dfe9f70c97c30f12ca41907a83034433993339
Arthur Gabriel <arthurgabriel933@gmail.com>
2021-12-01 20:52:17 -0300

    conectando ao bd

---
+++ src/applcation/Drive.java
@@ -0,0 +1,18 @@
+package applcation;
+
+import java.sql.*;
+
+public class Drive {
+
+	public static void main(String[] args) {
+		
+		private static final String URL = "jdbc:mysql://localhost:3306/tbl_20";
+		private static final String USER = "root";
+		private static final String PASSWRD = "1234";
+		
+		try {
+			
+		}
+	}
+
+}
