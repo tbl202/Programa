4130a807cfb4bd103e76d0fb11b226bfacc3e1ac
Arthur Gabriel <arthurgabriel933@gmail.com>
2021-12-01 21:20:01 -0300

    conectando ao bd 1.1

--- src/applcation/Drive.java
+++ src/applcation/Drive.java
@@ -4,15 +4,25 @@ import java.sql.*;
 
 public class Drive {
 
+	private static final String URL = "jdbc:mysql://localhost:3306/tbl_20";
+	private static final String USER = "root";
+	private static final String PASSWRD = "1234";
+	
 	public static void main(String[] args) {
 		
-		private static final String URL = "jdbc:mysql://localhost:3306/tbl_20";
-		private static final String USER = "root";
-		private static final String PASSWRD = "1234";
-		
 		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			Connection con = DriverManager.getConnection(URL, USER, PASSWRD);
 			
+			System.out.println("OK");
+			
+			con.close();
 		}
+	
+		catch (Exception ex) {
+			System.out.println("ERROR");
+		}
+	
 	}
 
 }
